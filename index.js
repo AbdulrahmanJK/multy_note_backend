e62a267235463473276ba053e1c3772dbb5fc112
Abdulrahman <rahman3000@mail.ru>
2023-11-23 21:16:34 +0300

    feat:added swagger

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const port = 4000;
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger/swagger.json');
 app.use(express.json());
 
 
 app.use(require('./routers/user.route'))
 app.use(require('./routers/note.route'))
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 mongoose
