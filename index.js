befba5ba5bb46d071e7d79f890b219151b4b0e11
Abdulrahman <rahman30002@gmail.com>
2024-07-05 10:23:11 +0300

    feat: add cors flags

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require('cors');
+
 const app = express()
+
+var whitelist = ['http://localhost:3000']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
+
+app.use(cors(corsOptions));
 const mongoose = require("mongoose")
+
 const port = 4000;
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
+
 app.use(express.json());
 
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -16,6 +38,10 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send("Something broke!");
 });
+
+
+
+
 
 mongoose
     .connect(process.env.SERVER)
